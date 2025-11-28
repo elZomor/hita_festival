9ee1c860787a40e3b043820a21568ebbcbec1210
zomor <mohamed.elzomor@outlook.com>
2025-11-28 04:46:00 +0200

    Show only if description has value

--- src/pages/show-detail/ShowInfoTab.tsx
+++ src/pages/show-detail/ShowInfoTab.tsx
@@ -6,12 +6,15 @@ type ShowInfoTabProps = {
 };
 
 export const ShowInfoTab = ({title, description}: ShowInfoTabProps) => (
-    <Card className="bg-gradient-to-br from-primary-50 to-white dark:from-primary-800 dark:to-primary-900" hover={false}>
-        <h2 className="text-2xl font-bold text-accent-600 dark:text-secondary-500 mb-4">
-            {title}
-        </h2>
-        <p className="text-primary-700 dark:text-primary-300 leading-relaxed text-lg">
-            {description}
-        </p>
-    </Card>
-);
+        description !== '' &&
+        (<Card className="bg-gradient-to-br from-primary-50 to-white dark:from-primary-800 dark:to-primary-900"
+               hover={false}>
+            <h2 className="text-2xl font-bold text-accent-600 dark:text-secondary-500 mb-4">
+                {title}
+            </h2>
+            <p className="text-primary-700 dark:text-primary-300 leading-relaxed text-lg">
+                {description}
+            </p>
+        </Card>)
+    )
+;
